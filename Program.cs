69371ec76e355751dea087bcaec4d238f4fa6a3c
Andre Buzelli <51280481+Buzelli@users.noreply.github.com>
2020-02-11 23:42:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Atividade_Compiladores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+  
+            System.IO.StreamReader file = new System.IO.StreamReader(@"D:\Programas_Algoritmos\C#\include.txt");
+            System.IO.StreamWriter res = new System.IO.StreamWriter(@"D:\Programas_Algoritmos\C#\resultado.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                if(line == "include")
+                {
+                    res.WriteLine("c = 250");
+                }
+                else
+                {
+                    res.WriteLine(line);
+                }
+               
+            }
+
+            file.Close();
+            res.Close();
+            System.Console.ReadLine();
+
+        }
+    }
+}
